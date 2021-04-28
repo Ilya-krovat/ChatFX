b451780c139ae1c1e57deea19a430fb67fbef6e2
Ilya <Mireech@yandex.ru>
2021-04-28 18:27:14 +0300

    first version

--- src/com/server/ChatOptions.java
+++ src/com/server/ChatOptions.java
@@ -2,6 +2,6 @@ package com.server;
 
 public class ChatOptions {
     public static int PORT = 8080;
-    public static String IP_ADDRESS = "192.168.0.105";
+    public static String IP_ADDRESS = "localhost";
 }
 
